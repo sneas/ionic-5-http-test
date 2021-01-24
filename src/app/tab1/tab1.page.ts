a49cdcdded71055d785eafcdb5269755d61437ff
Dima Snisarenko <snisarenkodima@gmail.com>
2021-01-24 09:57:59 +0100

    chore: add android builds

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -13,16 +13,16 @@ export class Tab1Page {
       .delete('https://httpbin.org/delete?with=%20wierd%20variables%20', {
         params: new HttpParams().append('a', '1').append('a', '2'),
       })
-      .subscribe(data => {
-        console.log('HttpClient', data);
+      .subscribe((data) => {
+        console.log('HttpClient', JSON.stringify(data));
       });
 
     this.http
       .sendRequest('https://httpbin.org/get?a=1&a=2', {
         method: 'get',
       })
-      .then(data => {
-        console.log('http data', JSON.parse(data.data));
+      .then((data) => {
+        console.log('http data', data.data);
       });
   }
 }
