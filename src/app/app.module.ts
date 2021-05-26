c439ea00ab332356afdd57fdc744445021ec3b40
Dima Snisarenko <snisarenkodima@gmail.com>
2021-05-26 06:22:16 +0200

    chore: install ionic-native-http-connection-backend v9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {IonicModule, IonicRouteStrategy, Platform} from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {
+  NativeHttpBackend,
+  NativeHttpFallback,
+  NativeHttpModule,
+} from 'ionic-native-http-connection-backend';
+import {HttpBackend, HttpClientModule, HttpXhrBackend} from "@angular/common/http";
+import { HTTP } from "@ionic-native/http/ngx";
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, NativeHttpModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    HTTP,
+    {
+      provide: HttpBackend,
+      useClass: NativeHttpFallback,
+      deps: [Platform, NativeHttpBackend, HttpXhrBackend],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
